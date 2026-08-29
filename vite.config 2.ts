7838b436b7cd1b713bcd21d460b900a81d136060
jac14014 <jac14014@gmail.com>
2026-08-28 19:44:37 -0500

    Copy complete site from lab

---
+++ vite.config 2.ts
@@ -0,0 +1,41 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        about: path.resolve(__dirname, "about.html"),
+        governance: path.resolve(__dirname, "governance.html"),
+        donate: path.resolve(__dirname, "donate.html"),
+        sitemap: path.resolve(__dirname, "sitemap.html"),
+        contact: path.resolve(__dirname, "contact.html"),
+        documents: path.resolve(__dirname, "documents.html"),
+        articlesOfIncorporation: path.resolve(__dirname, "articles-of-incorporation.html"),
+        bylaws: path.resolve(__dirname, "bylaws.html"),
+        orgChart: path.resolve(__dirname, "org-chart.html"),
+        memorandumRoot: path.resolve(__dirname, "memorandum.html"),
+        memorandumUsRoot: path.resolve(__dirname, "memorandum-us.html"),
+        amazonLabProgram: path.resolve(__dirname, "amazon-lab-program.html"),
+        cdriFunding: path.resolve(__dirname, "research/funding/cdri.html"),
+        reports: path.resolve(__dirname, "reports/index.html"),
+        memo: path.resolve(__dirname, "memo/index.html"),
+        memorandum: path.resolve(__dirname, "memo/memorandum.html"),
+        memorandumUs: path.resolve(__dirname, "memo/memorandum-us.html"),
+      },
+    },
+  },
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
